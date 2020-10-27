c1530cb8be9393904aabb7d2c84963f3386fe49f
Steven <schenn1992@gmail.com>
2020-10-27 00:00:51 +0000

    test result with assertEqual

--- head.js
+++ head.js
@@ -8,10 +8,14 @@ const assertEqual = function(actual, expected) {
 
 const head = function(array) {
   let itemHead;
-  if (array.length === 0){
+  if (array.length === 0) {
     itemHead = "undefined";
   } else {
     itemHead = array[0];
   }
   return itemHead;
 };
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
