1ea89d20887ed5285094a0956382d1ba5d17c191
Steven <schenn1992@gmail.com>
2020-11-02 21:05:43 +0000

    export middle func and require eqArrays and assertArraysEqual

--- middle.js
+++ middle.js
@@ -1,28 +1,6 @@
-const eqArrays = function(arrayA, arrayB) {
-  let result = false;
-  //use nested loop to iterate each items in each array
-  for (let i = 0; i <= arrayA.length - 1; i++) {
-    for (let j = 0; j <= arrayB.length - 1; j++) {
-      //check each value to determine if they match
-      if (arrayA[i] === arrayB[j]) {
-        result = true;
-      } else {
-        result = false;
-      }
-    }
-  }
-  return result;
-};
+const eqArrays = require("./eqArrays");
+const assertArraysEqual = require("./assertArraysEqual");
 
-const assertArraysEqual = function(arrayA, arrayB) {
-  if (eqArrays(arrayA, arrayB)) {
-    console.log(`🟢 Assertion Passed:  ${arrayA}  === ${arrayB}`);
-  } else {
-    console.log(`🔴 Assertion Failed: ${arrayA}  !== ${arrayB}`);
-  }
-};
-
-//Implement middle which will take in an array and return the middle-most element(s) of the given array.
 //For arrays with odd number of elements, an array containing a single middle element should be returned.
 //For arrays with an even number of elements, an array containing the two elements in the middle should be returned
 const middle = function(array) {
@@ -39,7 +17,7 @@ const middle = function(array) {
 
 }; // end of for loop
 
- 
-//console.log(middle([1, 2]))
-//console.log(middle([1, 2, 3, 4, 5])) // => [3]
-console.log(middle([1, 2, 3, 4])) // => [2, 3]
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]) // => [3]
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3,4]); // => [3,4]
+
+module.exports = middle;
