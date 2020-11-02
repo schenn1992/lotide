c0c41238dd0dbc0f37bca0ce53e98d6dffad1604
Steven <schenn1992@gmail.com>
2020-11-02 20:21:21 +0000

    mdoified code with requiring assertEqual and exporting eqArray

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log("Assertion Passed: " + actual + " === " + expected);
-  } else {
-    console.log("Assertion Failed: " + actual + " !== " + expected);
-  }
-};
+
+const assertEqual = require("./assertEqual");
 
 const eqArrays = function(arrayA, arrayB) {
   let result = false;
@@ -22,4 +17,5 @@ const eqArrays = function(arrayA, arrayB) {
   return result;
 };
 
-
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
+module.exports = eqArrays;
