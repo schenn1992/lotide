76c2137ea556d5cdfa815448bae9c7d283598073
Steven <schenn1992@gmail.com>
2020-11-02 19:21:19 +0000

    add module.exports

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,12 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("Assertion Passed: " + actual + " === " + expected);
+    console.log("🟢🟢🟢 Assertion Passed: " + actual + " === " + expected);
   } else {
-    console.log("Assertion Failed: " + actual + " !== " + expected);
+    console.log("🔴🔴🔴 Assertion Failed: " + actual + " !== " + expected);
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+//assertEqual("Lighthouse Labs", "Bootcamp");
+//assertEqual(1, 1);
+
+module.exports = assertEqual;
