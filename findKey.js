c001dbb18e41c3d56100510db14953fe96ab48ae
Steven <schenn1992@gmail.com>
2020-11-02 23:51:13 +0000

    export findKey

--- findKey.js
+++ findKey.js
@@ -17,12 +17,14 @@ const findKey = function(objectToTest, callback) {
   return undefined; 
 };
 
-assertEqual(findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2), "noma");
+module.exports = findKey;
+
+// assertEqual(findKey({
+//   "Blue Hill": { stars: 1 },
+//   "Akaleri":   { stars: 3 },
+//   "noma":      { stars: 2 },
+//   "elBulli":   { stars: 3 },
+//   "Ora":       { stars: 2 },
+//   "Akelarre":  { stars: 3 }
+// }, x => x.stars === 2), "noma");
 
