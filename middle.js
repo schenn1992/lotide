1beb4e4ef6686307fae64a94db8ae9dceae2e381
Steven <schenn1992@gmail.com>
2020-11-02 21:11:18 +0000

    edit unecessary comment

--- middle.js
+++ middle.js
@@ -17,7 +17,6 @@ const middle = function(array) {
 
 }; 
 
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]) // => [3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3,4]); // => [3,4]
+//assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]) // => [3]
 
 module.exports = middle;
