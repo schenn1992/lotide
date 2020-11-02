1b5219a6362e54a260eba22282ce2af8c54f095c
Steven <schenn1992@gmail.com>
2020-11-02 22:48:37 +0000

    including space

--- test/middleTest.js
+++ test/middleTest.js
@@ -8,6 +8,7 @@ const assert = require('chai').assert;
 const middle = require('../middle');
 
 describe("#middle", () => {
+  
   it("returns [3] for [1, 2, 3, 4, 5]", () => {
     assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
   });
