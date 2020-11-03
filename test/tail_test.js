9f09f1b75b3a720e49494bc5ecd378c2cbf483cd
Steven <schenn1992@gmail.com>
2020-11-03 03:10:50 +0000

    remove tail_test.js

--- test/tail_test.js
+++
@@ -1,6 +0,0 @@
-const tail = require("../tail.js")
-const assertEqual = require("../assertEqual.js")
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
