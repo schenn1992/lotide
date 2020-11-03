a7005ebc35985284afd3c466982633d5d631e91a
Steven <schenn1992@gmail.com>
2020-11-03 01:17:38 +0000

    add all functions to object

---
+++ index.js
@@ -0,0 +1,39 @@
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const without = require('./without');
+const takeUntil = require('./takeUntil');
+const letterPositions = require('./letterPositions');
+const findKeyByValue = require('./findKeyByValue');
+const findKey = require('./findKey');
+const map = require('./map');
+const reverseString = require('./reverse');
+const countLetters = require('./countLetters');
+const countOnly =  require('./countOnly');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const assertEqual = require('./assertEqual');
+const assertArraysEqual = require('./assertArraysEqual');
+
+
+module.exports = {
+  head:   head,
+  tail:   tail,
+  middle: middle,
+  wihtout: without,
+  takeUntil: takeUntil,
+  letterPositions: letterPositions,
+  findKeyByValue: findKeyByValue,
+  findKey: findKey,
+  map: map,
+  reverseString: reverseString,
+  countLetters: countLetters,
+  countOnly: countOnly,
+  eqArrays: eqArrays,
+  eqObjects: eqObjects,
+  assertObjectsEqual: assertObjectsEqual,
+  assertEqual: assertEqual,
+  assertArraysEqual: assertArraysEqual,
+};
+
